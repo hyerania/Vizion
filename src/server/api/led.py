02cb1490e95ee0b22ab51d69e8987ac9eb90a13a
tyler-nardecchia <tyler_nardecchia@tamu.edu>
2017-11-03 11:35:16 -0500

    simple led functionality

--- src/server/api/led.py
+++ src/server/api/led.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
+import RPi.GPIO as GPIO
 
 def turn_on_led(response):
     # code goes here
-
-
-
-
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(18, GPIO.OUT)
+	GPIO.output(18, 1)
 
     # if the function execute successfully, return true, otherwise return false
     return HttpResponse({'success': False})
