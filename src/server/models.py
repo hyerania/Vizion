45709861fb4e55c931aab4cd0f9e6fb544f915f6
Unknown <russell.pier@tamu.edu>
2017-11-15 13:15:36 -0600

    Add Db Models
    - Models are in server/models.py
    - must run command `python manage.py migrate` to get to work
    - also updated .gitignore to ignore the db file

---
+++ src/server/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin, models.Model):
+    identifier = models.CharField(max_length=40, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    # doorlock is ownership of a door
+    doorlock = models.IntegerField()
+    # access_locks is what they can access
+    access_locks = models.CommaSeparatedIntegerField(blank=True, max_length=255,)
+
+    USERNAME_FIELD = 'identifier'
+
+class Lock(models.Model):
+    address = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+class Transaction(models.Model):
+    date_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='transactions')
+    lock = models.ForeignKey('Lock', on_delete=models.CASCADE, related_name='transactions')
+    success = models.BooleanField()
+    access_type = models.SmallIntegerField
--- src/server/settings.py
+++ src/server/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server.api'
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'server.User'
 
 
 # Internationalization
