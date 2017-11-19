b93c71821ad03ff80319b953792ef01eb85f64ec
Yerania Hernandez <hernandez.yerania2@gmail.com>
2017-11-19 15:56:58 -0600

    Implemented Adding user access

--- src/app/VizionApp/app/src/main/java/csce462/vizionapp/AddUserAccess.java
+++ src/app/VizionApp/app/src/main/java/csce462/vizionapp/AddUserAccess.java
@@ -1,13 +1,30 @@
 package csce462.vizionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddUserAccess extends AppCompatActivity {
+    private Button btnAddUser;
+    private EditText etUsersEmailAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_access);
+
+        etUsersEmailAddress = (EditText) findViewById(R.id.etUsersEmailAddress);
+
+        btnAddUser = (Button) findViewById(R.id.btnAddUser);
+        btnAddUser.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //@TODO Must add information gathered to database and return to User Access with UPDATED information
+                Intent addIntent = new Intent(AddUserAccess.this, UsersAccess.class);
+                startActivity(addIntent);
+            }
+        });
     }
 }
